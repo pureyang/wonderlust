b6180418d088ad24bafe7054348b30f3d5f8db06
Carolyn Yang <carolyn.x.yang@gmail.com>
2015-11-05 00:12:42 -0800

    Making changes so that we can have multiple panels shifting with main scroll nav on top

---
+++ wonderlust/StageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StageViewController.swift
+//  wonderlust
+//
+//  Created by Carolyn Yang on 11/4/15.
+//  Copyright © 2015 Yang Tang. All rights reserved.
+//
+
+import UIKit
+
+class StageViewController: UIViewController,UIScrollViewDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+}
