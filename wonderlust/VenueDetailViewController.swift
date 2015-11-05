b945b34733109693eb3678348f1e1a40e491aca5
Carolyn Yang <carolyn.x.yang@gmail.com>
2015-11-04 23:01:52 -0800

    Adding slide image

--- wonderlust/VenueDetailViewController.swift
+++ wonderlust/VenueDetailViewController.swift
@@ -8,21 +8,52 @@
 
 import UIKit
 
-class VenueDetailViewController: UIViewController {
+class VenueDetailViewController: UIViewController,UIScrollViewDelegate {
 
-    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var textScrollView: UIScrollView!
+    @IBOutlet weak var pictureScroll: UIScrollView!
+    @IBOutlet weak var pictureContainer: UIView!
+    
+    @IBOutlet weak var imagePage: UIPageControl!
+    @IBOutlet weak var detailText: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        // hardcoded now
-        scrollView.contentSize = CGSize(width: 375, height: 1100)
+        textScrollView.contentSize = detailText.frame.size
+        textScrollView.contentSize.height+=375
+        // Adding images
+        var image:UIImage
+        var imageView:UIImageView
+        var location:CGFloat = 0
+
+        for i in 1...5 {
+            image = UIImage(named: "L\(i)")!
+            imageView = UIImageView(image:image)
+            imageView.frame = CGRect(x: location, y: 0, width: image.size.width, height: image.size.height)
+            location += image.size.width
+            pictureContainer.addSubview(imageView)
+        }
+        pictureScroll.contentSize = CGSize(width: location, height: 375)
+        imagePage.numberOfPages = 5
+        imagePage.currentPage = 0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.frame.size.width
+        self.imagePage.currentPage  = Int(round(page))
+
+    }
+    @IBAction func backClick(sender: UIButton) {
+        performSegueWithIdentifier("toPickerView", sender: self)
     }
     
 
