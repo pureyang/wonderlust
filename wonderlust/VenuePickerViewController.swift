4e85574e789cd884770feebdb98edafaf4bca47c
pureyang <pureyang@gmail.com>
2015-11-08 22:02:33 -0800

    carbasic card swiping completed

--- wonderlust/VenuePickerViewController.swift
+++ wonderlust/VenuePickerViewController.swift
@@ -7,58 +7,52 @@
 //
 
 import UIKit
+import Koloda
+import pop
 
-class VenuePickerViewController: UIViewController {
+private var numberOfCards: UInt = 3
 
-    var isMenuExpanded = false
+class VenuePickerViewController: UIViewController, KolodaViewDataSource, KolodaViewDelegate {
 
-    @IBOutlet weak var pickerView: UIView!
-    
-    @IBOutlet weak var place1ImageView: UIImageView!
-    @IBOutlet weak var place1DetailImageView: UIImageView!
-    
-    var pickerViewOriginalCenter: CGPoint!
+    @IBOutlet weak var kolodaView: KolodaView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        // pickerViewOriginalCenter = pickerView.center
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
+        
+        self.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        
+    }
+    
+    //MARK: KolodaViewDataSource
+    func kolodaNumberOfCards(koloda: KolodaView) -> UInt {
+        return numberOfCards
+    }
+    
+    func kolodaViewForCardAtIndex(koloda: KolodaView, index: UInt) -> UIView {
+        return UIImageView(image: UIImage(named: "Card\(index + 1)"))
+    }
+    
+    func kolodaViewForCardOverlayAtIndex(koloda: KolodaView, index: UInt) -> OverlayView? {
+        return NSBundle.mainBundle().loadNibNamed("OverlayView",
+            owner: self, options: nil)[0] as? OverlayView
+    }
+    
+    //MARK: KolodaViewDelegate
+    
+    func kolodaDidSwipedCardAtIndex(koloda: KolodaView, index: UInt, direction: SwipeResultDirection) {
+        //Example: loading more cards
+        if index >= 3 {
+            numberOfCards = 6
+            kolodaView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    @IBAction func onMenuTap(sender: AnyObject) {
-        
-        if (!isMenuExpanded) {
-            // collapse menu
-            UIView.animateWithDuration(0.3, animations: { () -> Void in
-                self.pickerView.center = CGPoint(x: self.pickerViewOriginalCenter.x+100, y: self.pickerViewOriginalCenter.y)
-                }, completion: { (finished) -> Void in
-                    self.isMenuExpanded = true;
-            })
-        } else {
-            // animate menu out
-            UIView.animateWithDuration(0.3, animations: { () -> Void in
-                self.pickerView.center = CGPoint(x: self.pickerViewOriginalCenter.x, y: self.pickerViewOriginalCenter.y)
-                }, completion: { (finished) -> Void in
-                    self.isMenuExpanded = false;
-            })
-        }
-        
     }
 }
